386d8826405d1c865f6494956880a37f9a3e86ca
MuhammadBilal-arch <expbilal@gmail.com>
2023-11-13 22:43:03 +0500

    fixed the reset card

--- src/pages/Authentication/resetPasswordEmail.tsx
+++ src/pages/Authentication/resetPasswordEmail.tsx
@@ -39,75 +39,77 @@ export const ResetPasswordEmail = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: '', 
+      email: '',
     },
     validationSchema,
     onSubmit: async (values) => {
-      navigate('/reset-password-otp')
+      navigate('/reset-password-otp');
     },
   });
 
   return (
-    <div className="p-2 md:p-4 max-h-screen flex flex-col h-screen justify-between">
+    <div className="flex relative h-screen max-h-screen flex-col justify-between p-2 md:p-4">
       <div
         style={{
           backgroundImage: `url(${ASSETS.AUTH.RESET_PASSWORD_COVER})`,
         }}
-        className="h-1/2 w-full bg-top bg-no-repeat md:bg-cover rounded-3xl"
+        className="h-1/2 w-full rounded-3xl bg-top bg-no-repeat md:bg-cover"
       >
-        <div className="mb-10 pt-5 xl:py-5 2xl:py-5 px-5 text-base font-semibold text-black-primary sm:text-lg md:text-xl lg:text-2xl">
+        <div className="mb-10 px-5 pt-5 text-base font-semibold text-black-primary sm:text-lg md:text-xl lg:text-2xl xl:py-5 2xl:py-5">
           Hairstyl
         </div>
-        <div className="flex flex-col items-center justify-center space-y-6">
-          <img src={ASSETS.AUTH.SEARCH_SELECT_STYL} alt="" className="h-14" />
-          <div className="flex h-full w-full flex-col justify-center space-y-14 rounded-tl-xl rounded-br-xl bg-white px-5 pb-10 shadow-equal sm:w-80">
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex w-full flex-col   text-black-primary"
-            >
-              <div className="my-5 flex w-full flex-col space-y-2">
-                <h1 className=" w-full  text-xl font-bold md:text-2xl">
-                  Reset Password
-                </h1>
-                <div className='text-xs md:text-sm'>Enter your email to reset password</div>
+      </div>
+      <div className="flex flex-col absolute px-5 sm:px-0 left-0 top-[15%] lg:top-[25%] 2xl:top-[35%] 3xl:top-[40%] right-0 items-center justify-center space-y-6">
+        <img src={ASSETS.AUTH.SEARCH_SELECT_STYL} alt="" className="h-14" />
+        <div className="flex h-full w-full flex-col justify-center space-y-14 rounded-tl-xl rounded-br-xl bg-white px-5 pb-10 shadow-equal sm:w-80">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex w-full flex-col   text-black-primary"
+          >
+            <div className="my-5 flex w-full flex-col space-y-2">
+              <h1 className=" w-full  text-xl font-bold md:text-2xl">
+                Reset Password
+              </h1>
+              <div className="text-xs md:text-sm">
+                Enter your email to reset password
               </div>
-              <div className="w-full space-y-2 2xl:space-y-3">
-                <InputWithLabel
-                  label="Email"
-                  placeholder="Your email address"
-                  type="email"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  errors={formik?.errors?.email}
-                  touched={formik?.touched?.email}
-                  style={true}
+            </div>
+            <div className="w-full space-y-2 2xl:space-y-3">
+              <InputWithLabel
+                label="Email"
+                placeholder="Your email address"
+                type="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                errors={formik?.errors?.email}
+                touched={formik?.touched?.email}
+                style={true}
+              />
+
+              <div className="space-y-5 pt-4">
+                <BtnFilled
+                  text="Send OTP"
+                  background="bg-yellow-primary"
+                  color="text-black-primary"
+                  width="w-full"
                 />
 
-                <div className="space-y-5 pt-4">
-                  <BtnFilled
-                    text="Send OTP"
-                    background="bg-yellow-primary"
-                    color="text-black-primary"
-                    width="w-full"
-                  />
-
-                  <div className="space-y-4 px-8 text-black-primary">
-                    <div
-                      onClick={() => navigate('/')}
-                      className="cursor-pointer text-center text-xs text-black-primary sm:text-sm md:text-base"
-                    >
-                      <span className="font-semibold">Sign in instead</span>
-                    </div>
+                <div className="space-y-4 px-8 text-black-primary">
+                  <div
+                    onClick={() => navigate('/')}
+                    className="cursor-pointer text-center text-xs text-black-primary sm:text-sm md:text-base"
+                  >
+                    <span className="font-semibold">Sign in instead</span>
                   </div>
                 </div>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
-      <div className="text-center md:text-right text-xs md:text-sm font-semibold text-gray-normal">
+      <div className="text-center text-xs font-semibold text-gray-normal md:text-right md:text-sm">
         @ 2023, Made with ❤️ by{' '}
         <span className="font-semibold text-yellow-primary">KST Team</span> for
         a better web.
--- src/pages/Authentication/resetPasswordOtp.tsx
+++ src/pages/Authentication/resetPasswordOtp.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
- 
+
 import { onLoginUser } from '../../redux/slices/user/user';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
@@ -16,12 +16,12 @@ export const ResetPasswordOtp = () => {
     (state: any) => state.User
   );
 
-  const [otp, setotp] = useState(); 
+  const [otp, setotp] = useState();
 
   const onEnterCode = async (value) => {
     setotp(value);
-    if (value.length === 5) { 
-      navigate('/reset-password')
+    if (value.length === 5) {
+      navigate('/reset-password');
     }
   };
 
@@ -64,65 +64,65 @@ export const ResetPasswordOtp = () => {
         style={{
           backgroundImage: `url(${ASSETS.AUTH.RESET_PASSWORD_COVER})`,
         }}
-        className="h-1/2 w-full bg-top bg-no-repeat md:bg-cover rounded-3xl"
+        className="h-1/2 relative w-full rounded-3xl bg-top bg-no-repeat md:bg-cover"
       >
         <div className="mb-10 px-5 pt-5 text-base font-semibold text-black-primary sm:text-lg md:text-xl lg:text-2xl xl:py-5 2xl:py-5">
           Hairstyl
         </div>
-        <div className="flex flex-col items-center justify-center space-y-6">
-          <img src={ASSETS.AUTH.SEARCH_SELECT_STYL} alt="" className="h-14" />
-          <div className="flex h-full w-full flex-col justify-center space-y-14 rounded-tl-xl rounded-br-xl bg-white px-5 pb-10 shadow-equal sm:w-80">
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex w-full flex-col  text-black-primary"
-            >
-              <div className="my-5 flex w-full flex-col space-y-2">
-                <h1 className=" w-full  text-xl font-bold md:text-2xl">
-                  Reset Password
-                </h1>
-                <div className="text-xs md:text-sm">
-                  Enter your 5 digits one time password
-                </div>
+      </div>
+      <div className="flex flex-col absolute px-5 sm:px-0 left-0 top-[15%] lg:top-[25%] 2xl:top-[35%] 3xl:top-[40%] right-0 items-center justify-center space-y-6">
+        <img src={ASSETS.AUTH.SEARCH_SELECT_STYL} alt="" className="h-14" />
+        <div className="flex h-full w-full flex-col justify-center space-y-14 rounded-tl-xl rounded-br-xl bg-white px-5 pb-10 shadow-equal sm:w-80">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex w-full flex-col  text-black-primary"
+          >
+            <div className="my-5 flex w-full flex-col space-y-2">
+              <h1 className=" w-full  text-xl font-bold md:text-2xl">
+                Reset Password
+              </h1>
+              <div className="text-xs md:text-sm">
+                Enter your 5 digits one time password
               </div>
-              <div className="w-full space-y-2 2xl:space-y-3">
-                <OTPInput
-                  containerStyle={{ width: '100%' }}
-                  value={otp}
-                  // isInputNum={true}
-                  onChange={onEnterCode}
-                  numInputs={5}
-                  renderInput={(props) => <input {...props} />}
-                  inputStyle={{
-                    width: '45px',
-                    height: '45px',
-                    border: '1px solid #D9D9D9',
-                    borderRadius: '10%',
-                  }}
-                  renderSeparator={
-                    <span className="w-1 text-transparent sm:w-3">-</span>
-                  }
+            </div>
+            <div className="w-full space-y-2 2xl:space-y-3">
+              <OTPInput
+                containerStyle={{ width: '100%' }}
+                value={otp}
+                // isInputNum={true}
+                onChange={onEnterCode}
+                numInputs={5}
+                renderInput={(props) => <input {...props} />}
+                inputStyle={{
+                  width: '45px',
+                  height: '45px',
+                  border: '1px solid #D9D9D9',
+                  borderRadius: '10%',
+                }}
+                renderSeparator={
+                  <span className="w-1 text-transparent sm:w-3">-</span>
+                }
+              />
+
+              <div className="space-y-5 pt-4">
+                <BtnFilled
+                  text="Next"
+                  background="bg-yellow-primary"
+                  color="text-black-primary"
+                  width="w-full"
                 />
 
-                <div className="space-y-5 pt-4">
-                  <BtnFilled
-                    text="Next"
-                    background="bg-yellow-primary"
-                    color="text-black-primary"
-                    width="w-full"
-                  />
-
-                  <div className="space-y-4 px-8 text-black-primary">
-                    <div
-                      onClick={() => navigate('/')}
-                      className="cursor-pointer text-center text-xs text-black-primary sm:text-sm md:text-base"
-                    >
-                      <span className="font-semibold">Sign in instead</span>
-                    </div>
+                <div className="space-y-4 px-8 text-black-primary">
+                  <div
+                    onClick={() => navigate('/')}
+                    className="cursor-pointer text-center text-xs text-black-primary sm:text-sm md:text-base"
+                  >
+                    <span className="font-semibold">Sign in instead</span>
                   </div>
                 </div>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
       <div className="text-center text-xs font-semibold text-gray-normal md:text-right md:text-sm">
